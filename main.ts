a76331a235dedd8a7c0306546c0e2a4b0ecd102b
yuuhrizin <155904795+yuuhrizin@users.noreply.github.com>
2024-03-05 16:29:25 -0300

    add Atividade

---
+++ main.ts
@@ -0,0 +1,12 @@
+namespace atividade {
+    function multiplicacao(num1: number, num2: number): number {
+        return num1 * num2
+    }
+    
+    function olaNome(nome: string): string {
+        return `Olá ${nome}`
+    }
+    
+    console.log(multiplicacao(10, 20))
+    console.log(olaNome('Yuri'))
+}
